652ff596f498ce0813f7a9c729af572e783ed878
Yer Thao <maivyerthao@gmail.com>
2020-11-06 16:46:22 -0600

    Added input and a button

---
+++ client.js
@@ -0,0 +1,26 @@
+console.log('js');
+$(document).ready(readyNow);
+
+function readyNow(){
+    console.log('jq');
+    
+}
+
+
+
+
+
+
+// The application should have an input form that collects _
+// employee first name, last name, ID number, job title, annual salary_.
+
+
+// A 'Submit' button should collect the form information, store the information 
+// to calculate monthly costs, append information to the DOM and clear the input 
+// fields. Using the stored information, calculate monthly costs and append this 
+// to the to DOM. If the total monthly cost exceeds $20,000, add a red background 
+// A 'Submit' button should collect the form information, store the information to 
+// calculate monthly costs, append information to the DOM and clear the input fields. 
+// Using the stored information, calculate monthly costs and append this to the to DOM. 
+// If the total monthly cost exceeds $20,000, add a red background color to the total 
+// monthly cost.color to the total monthly cost.
