d273b8330a2cb16c1905ccca1d99ea5631369f55
Yer Thao <maivyerthao@gmail.com>
2020-11-06 17:03:29 -0600

    Added input fields and button

--- client.js
+++ client.js
@@ -14,6 +14,7 @@ function readyNow(){
 // The application should have an input form that collects _
 // employee first name, last name, ID number, job title, annual salary_.
 
+////////////////////////////////////////////////////////////////////////////////////
 
 // A 'Submit' button should collect the form information, store the information 
 // to calculate monthly costs, append information to the DOM and clear the input 
@@ -24,3 +25,7 @@ function readyNow(){
 // Using the stored information, calculate monthly costs and append this to the to DOM. 
 // If the total monthly cost exceeds $20,000, add a red background color to the total 
 // monthly cost.color to the total monthly cost.
+
+////////////////////////////////////////////////////////////////////////////////////
+
+
