a0f33ff3015d48841d98b3e0ef802d62e0d54d6a
Yer Thao <maivyerthao@gmail.com>
2020-11-07 16:43:47 -0600

    Created button on table

--- client.js
+++ client.js
@@ -5,12 +5,13 @@ function readyNow(){
     console.log('jq');
     // on the click of the subButton, we run the submitInfo function
     $('#btn-add').on('click', submitInfo);
-
 }
 
 
 
 function submitInfo(){
+    // this method will stop page from page refresh as per lecture on Friday from Mary
+    event.preventDefault()
     console.log('in submitInfo:'); // 
 // created object variable to store the value of each input box into
 // so that we can grab the object full of all values instead of
@@ -37,23 +38,25 @@ function submitInfo(){
 console.log('Testing submitInfo funct');
 
 function append(employee){
-    $('#table-content').append(`<th class="trow" id="fName">${employee.firstName}</th>`);
-    $('#table-content').append(`<th class="trow" id="fName">${employee.lastName}</th>`);
-    $('#table-content').append(`<th class="trow" id="fName">${employee.idNum}</th>`);
-    $('#table-content').append(`<th class="trow" id="fName">${employee.title}</th>`);
-    $('#table-content').append(`<th class="trow" id="fName">${employee.salary}</th>`);
-
-
+    $('#table-content').append(`
+            <tr>
+                <td id="fName">${employee.firstName}</td>
+                <td id="lName">${employee.lastName}</td>
+                <td id="iName">${employee.idNum}</td>
+                <td id="tName">${employee.title}</td>
+                <td id="sName">${employee.salary}</td>
+                <th><button id="dButton">Delete</button></th>
+            </tr>`);
 }
 
 
 // This function's goal is to set the values of the input fields back to empty strings
 function emptyInput(){
-    $('#firstInput').val(''),
-    $('#lastInput').val('')
-    $('#idInput').val('')
-    $('#titleInput').val('')
-    $('#salaryInput').val('')
+    $('#firstInput').val('');
+    $('#lastInput').val('');
+    $('#idInput').val('');
+    $('#titleInput').val('');
+    $('#salaryInput').val('');
 }
 
 
@@ -102,11 +105,29 @@ function emptyInput(){
 - created 5 input elements with different ids 
 - created second div to contain button element alone
 - created third div, created class element named container to embody
-- entire 
+entire table elements 
+- created table tags, thead (table head), h3 tag for table name Employee, 
+- created tr tag - which is an element that holds the th (in table headers)
+- created th tags inside the in-head headers : First Name, Last Name... etc
+- closed tr tag
+- closed thead tag - this closes the element on table for in table header 
+- created table body tag to hold following:
+    - tr
 
 
 
-*/
+
+
+
+
+
+
+
+
+
+    
+
+ */
 ////////////////////////////////////////////////////////////////////////////////////
 
 
